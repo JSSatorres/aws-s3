f334e46249de433e54329482bd5139acd83f93e2
jssatorres <juansataz.dev@gmail.com>
2023-01-23 14:51:44 +0100

    Feat (Server) adding temporal path

--- src/server.js
+++ src/server.js
@@ -1,14 +1,12 @@
 import express  from "express"
 import 'dotenv/config' 
-import cors from 'cors'
 import fileUpload from "express-fileupload"
-import foodStarsRouter from './routes/files.routes.js'
 import filesRouter from './routes/files.routes.js'
+// import cors from 'cors'
 
 const app = express()
 
 const whiteList = [process.env.ORIGIN1,process.env.ORIGIN2]
-
 
 // app.use(cors({
 //   origin: (origin, callback) => {
@@ -19,13 +17,12 @@ const whiteList = [process.env.ORIGIN1,process.env.ORIGIN2]
 //   },
 // }));
 
-// app.post('/files', function(req, res) {
-//   console.log(req);
-//   console.log(req.files.foo); // the uploaded file object
-// });
-app.use(fileUpload());
+app.use(fileUpload({
+  useTempFiles:true,
+  tempFileDir:'./uploads'
+}));
+
 // app.use(express.json());
-// app.use("/foodstars",foodStarsRouter);
 app.use("/files",filesRouter);
 
 export default app

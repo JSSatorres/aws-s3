3d4d933bcfacc93b133a97cee5e8088cea266b01
jssatorres <juansataz.dev@gmail.com>
2023-01-23 14:09:43 +0100

    Feat  (Varibales) adding variables

---
+++ src/config/config.js
@@ -0,0 +1,9 @@
+import {config} from 'dotenv'
+
+export const AWS_BUCKET_NAME = process.env.AWS_BUCKET_NAME
+export const AWS_BUCKET_REGION = process.env.AWS_BUCKET_REGION
+export const AWS_USER_PASSWORD = process.env.AWS_USER_PASSWORD
+export const AWS_USER = process.env.AWS_USER
+export const AWS_START_CONSOLE = process.env.AWS_START_CONSOLE
+export const AWS_PUBLIC_KEY = process.env.AWS_PUBLIC_KEY
+export const AWS_SECRET_ACCESS_KEY = process.env.AWS_SECRET_ACCESS_KEY
